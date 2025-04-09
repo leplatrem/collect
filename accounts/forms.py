7c75c476a9191149d9d34c29c15e950c446557e6
Mathieu Leplatre <mathieu@mozilla.com>
2025-04-09 11:25:17 +0200

    Remove usable_password field from signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class SignUpForm(UserCreationForm):
+    usable_password = None  # Remove this unused field.
     secret = forms.CharField(label=_("Invitation secret"), max_length=30, required=True)
 
     def clean_secret(self):
