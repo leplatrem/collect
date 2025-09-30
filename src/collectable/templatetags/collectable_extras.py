2a08c541cff4835d73604ceed8c2f9ae5f4dec3a
Mathieu Leplatre <mathieu@leplat.re>
2025-10-01 00:16:01 +0200

    Fix #15: Add OG preview

--- src/collectable/templatetags/collectable_extras.py
+++ src/collectable/templatetags/collectable_extras.py
@@ -22,3 +22,16 @@ def user_possession_form(context, collectable):
 def next_page_reveal_index(value):
     # Start loading the next page when second half of current page is revealed.
     return int(len(value) * settings.PAGE_REVEAL_LOAD_NEXT)
+
+
+@register.simple_tag(takes_context=True)
+def fullurl(context, path=""):
+    """
+    Returns the full absolute URL.
+    """
+    # requires 'django.template.context_processors.request' context processor
+    request = context["request"]
+    # Ensure path starts with '/'
+    if not path.startswith("/"):
+        path = "/" + path
+    return request.build_absolute_uri(path)
