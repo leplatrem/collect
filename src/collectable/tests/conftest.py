bd6eea202af4b5f515a995af9064c82d9439aa89
Mathieu Leplatre <mathieu@mozilla.com>
2026-09-20 21:31:23 +0200

    Move conftest to root

--- src/collectable/tests/conftest.py
+++ src/collectable/tests/conftest.py
@@ -4,7 +4,6 @@ import tempfile
 import pytest
 from django.conf import settings
 from django.core.management import call_command
-from django.utils import translation
 
 from collectable.tests.factories import (
     CollectableFactory,
@@ -23,13 +22,6 @@ def staticfiles():
         yield
     finally:
         shutil.rmtree(static_root)
-
-
-@pytest.fixture(autouse=True, scope="session")
-def set_language():
-    translation.activate("fr")
-    yield
-    translation.deactivate()
 
 
 @pytest.fixture
